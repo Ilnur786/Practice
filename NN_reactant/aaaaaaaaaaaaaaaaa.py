2009de74f4c0c20062259dc712d46f401911a252
Ilnur786 <Ilnur-qwerty@mail.ru>
2020-05-08 00:38:45 +0300

    last results

--- NN_reactant/aaaaaaaaaaaaaaaaa.py
+++ NN_reactant/aaaaaaaaaaaaaaaaa.py
@@ -1,31 +1,11 @@
 from pickle import load
 
-with open("test_train/intersection_dict.pickle", 'rb') as f:
-    intersection_dict = load(f)
+with open("false_base/false_test_set_part2.pickle", 'rb') as f:
+    var = load(f)
 
-with open('test_train/false_test_molecules_ids.pickle', 'rb') as f:
-    false_test_molecules_ids = load(f)
+print(len(var))
 
-with open('test_train/false_train_molecules_ids.pickle', 'rb') as f:
-    false_train_molecules_ids = load(f)
+with open("test_train/false_test_set.pickle", 'rb') as f:
+    c = load(f)
 
-with open('test_train/true_train_molecules_ids.pickle', 'rb') as f:
-    true_train_molecules_ids = load(f)
-
-with open('test_train/true_test_molecules_ids.pickle', 'rb') as f:
-    true_test_molecules_ids = load(f)
-
-if false_train_molecules_ids & true_train_molecules_ids:
-    print('a')
-if false_test_molecules_ids & true_test_molecules_ids:
-    print('b')
-if false_test_molecules_ids & true_train_molecules_ids:
-    print('c')
-if false_test_molecules_ids & false_train_molecules_ids:
-    print('d')
-if false_train_molecules_ids & true_test_molecules_ids:
-    print('e')
-if false_train_molecules_ids & false_test_molecules_ids:
-    print('f')
-
-print(len(false_test_molecules_ids))
+print(type(c))
