5171b38e3ce97dc75b7a43eca74af972cd6b32a9
Ilnur786 <Ilnur-qwerty@mail.ru>
2020-07-07 16:31:21 +0300

    I corrected some mistakes in files

--- NN_reactant/false_base.py
+++ NN_reactant/false_base.py
@@ -1,148 +1,113 @@
-#Этот код предназначен для разделения базы негативных примеров на тест и трейн , но работает он с тюплами в которых индексы молекул ( и то со старой базой позитивных примеров которые были сделаны на основе реакций, а не правил)
+# Этот код предназначен для разделения базы негативных примеров на тест и трейн (работает с реальной базой и с тюплами в которых молекул контейнеры)
 from pickle import dump, load
-import logging
 from time import time
 
-#false
 start = time()
-logger = logging.getLogger("exampleApp")
-logger.setLevel(logging.INFO)
 
-# create the logging file handler
-fh = logging.FileHandler("test_train/false_train_test.log")
+with open('uspto/true_base/true_test_molecules_signatures.pickle', 'rb') as f:
+    true_test_molecules_signatures = load(f)
 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
+with open('uspto/true_base/true_train_molecules_signatures.pickle', 'rb') as q:
+    true_train_molecules_signatures = load(q)
 
-# add handler to logger object
-logger.addHandler(fh)
+def divide_chunks(o, n):
+    o = list(o)
+    for z in range(0, len(o), n):
+        yield o[z:z + n]
 
-logger.info("Program started")
-
-with open("test_train/false_base_ids.pickle", 'rb') as f:
-    pickle_file = load(f)
-print('загрузился файл false_base_ids.pickle ')
-
-with open("test_train/true_train_molecules_ids.pickle", 'rb') as f:
-    true_train_molecules_ids = load(f)
-print('загрузился файл true_train_molecules_ids.pickle ')
-
-with open("test_train/true_test_molecules_ids.pickle", 'rb') as f:
-    true_test_molecules_ids = load(f)
-print('загрузился файл true_test_molecules_ids.pickle ')
-
-pickle_file = list(pickle_file)
+false_train_molecules_signatures = set()
+false_test_molecules_signatures = set()
 false_test_set = set()
-print('в false_test_set добавлены 1000 тюплов')
-
 false_train_set = set()
-false_test_molecules_ids = set()
+false_lost_molecules_signatures = set()
+false_lost_set, y, u, p = 0, 0, 0, 0
+a, c = 1, 1
 
-false_train_molecules_ids = set()
-print('в false_test_molecules_ids хранятся молекулы из тюплов false_test_set.pickle')
-
-false_lost_set = set()
-false_lost_molecules_ids = set()
-a = 0
-b = 0
-c = 0
-intersection_dict = {}
-
-for i, kortezh in enumerate(set(pickle_file)):
-    if i > 0 and i % 1000 == 0:
-        if false_train_molecules_ids & false_test_molecules_ids:
-            a += 1
-            print('есть пересечение aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-            logger.info('есть пересечение aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-        elif false_train_molecules_ids & true_test_molecules_ids:
-            b += 1
-            print('есть пересечение bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')
-            logger.info('есть пересечение bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')
-        elif false_test_molecules_ids & true_train_molecules_ids:
-            intersection_dict[i] = kortezh
-            c += 1
-            print('есть пересечение cccccccccccccccccccccccccccccccccccccccccccccccccc')
-            logger.info('есть пересечение cccccccccccccccccccccccccccccccccccccccccccc')
-    if set(kortezh) & false_train_molecules_ids or set(kortezh) & false_test_molecules_ids or set(kortezh) & true_train_molecules_ids or set(kortezh) & true_test_molecules_ids:
-        if set(kortezh) & false_train_molecules_ids and (not set(kortezh) & false_test_molecules_ids) and (not set(kortezh) & true_test_molecules_ids):
-            false_train_set.add(kortezh)
-            false_train_molecules_ids.update(kortezh)
-        # elif (not set(kortezh) & false_train_molecules_ids) and (not set(kortezh) & true_train_molecules_ids) and (set(kortezh) & false_test_molecules_ids or set(kortezh) & true_test_molecules_ids):
-        #     false_test_set.add(kortezh)
-        #     false_test_molecules_ids.update(kortezh)
-        elif set(kortezh) & false_test_molecules_ids and (not set(kortezh) & false_train_molecules_ids) and (not set(kortezh) & true_train_molecules_ids):
-            if set(kortezh) & true_train_molecules_ids:
-                continue
-            else:
+for i in range(1, 918):
+    print('номер файла', i)
+    with open(f'uspto/new_false_ATB/{i}.pickle', 'rb') as r:
+        pickle_file = load(r)
+    for j, kortezh in enumerate(pickle_file):
+        print('номер кортежа', j)
+        r1, t, r2, _ = kortezh
+        tmp = {bytes(r1), bytes(t), bytes(r2)}
+        if (i > 0 and i % 50 == 0) or (i == 917 and j == len(pickle_file) - 1):
+            if false_train_molecules_signatures & false_test_molecules_signatures:
+                y += 1
+                print('есть пересечение yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy')
+            elif false_train_molecules_signatures & true_test_molecules_signatures:
+                u += 1
+                print('есть пересечение uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu')
+            elif false_test_molecules_signatures & true_train_molecules_signatures:
+                p += 1
+                print('есть пересечение pppppppppppppppppppppppppppppppppppppppppppppp')
+        if tmp & false_train_molecules_signatures or tmp & false_test_molecules_signatures or tmp & true_train_molecules_signatures or tmp & true_test_molecules_signatures:
+            if tmp & false_train_molecules_signatures and (not tmp & false_test_molecules_signatures) and (not tmp & true_test_molecules_signatures):
+                false_train_set.add(kortezh)
+                false_train_molecules_signatures.update(tmp)
+            elif tmp & false_test_molecules_signatures and (not tmp & false_train_molecules_signatures) and (not tmp & true_train_molecules_signatures):
                 false_test_set.add(kortezh)
-                false_test_molecules_ids.update(kortezh)
-        elif set(kortezh) & true_test_molecules_ids and (not set(kortezh) & true_train_molecules_ids) and (not set(kortezh) & false_train_molecules_ids):
-            if set(kortezh) & true_train_molecules_ids:
-                continue
-            else:
+                false_test_molecules_signatures.update(tmp)
+            elif tmp & true_test_molecules_signatures and (not tmp & true_train_molecules_signatures) and (not tmp & false_train_molecules_signatures):
                 false_test_set.add(kortezh)
-                false_test_molecules_ids.update(kortezh)
-        elif set(kortezh) & true_train_molecules_ids and (not set(kortezh) & true_test_molecules_ids) and (not set(kortezh) & false_test_molecules_ids):
-            false_train_set.add(kortezh)
-            false_train_molecules_ids.update(kortezh)
+                false_test_molecules_signatures.update(tmp)
+            elif tmp & true_train_molecules_signatures and (not tmp & true_test_molecules_signatures) and (not tmp & false_test_molecules_signatures):
+                false_train_set.add(kortezh)
+                false_train_molecules_signatures.update(tmp)
+            else:
+                false_lost_set += 1
+                false_lost_molecules_signatures.update(tmp)
         else:
-            false_lost_set.add(kortezh)
-            false_lost_molecules_ids.update(kortezh)
-    else:
-        false_test_set.add(kortezh)
-        false_test_molecules_ids.update(kortezh)
-    print('номер кортежа', i)
-print('в false_train_set добавились все кортежи из false_base_ids.pickle, молекулы которых не входят false_test_molecules_ids')
-print('в false_test_set добавились все кортежи из false_base_ids.pickle, молекулы которых не входят false_train_molecules_ids и в true_train_molecules_ids')
-logger.info('пересекалось молекул false_train_molecules_ids & false_test_molecules_ids: {}'.format(a))
-logger.info('пересекалось молекул false_train_molecules_ids & true_test_molecules_ids: {}'.format(b))
-logger.info('пересекалось молекул false_test_molecules_ids & true_train_molecules_ids: {}'.format(c))
+            false_test_set.add(kortezh)
+            false_test_molecules_signatures.update(tmp)
+        if len(false_test_set) >= 5000:
+            for chunk in divide_chunks(false_test_set, 5000):
+                if len(chunk) < 5000:
+                    false_test_set = set(chunk)
+                else:
+                    print('dumping...', a)
+                    with open(f'uspto/false_base/false_test_set/{a}.pickle', 'wb') as f:
+                        dump(chunk, f)
+                    print('dumping successful!')
+                    false_test_set = set()
+                    a += 1
+        if len(false_train_set) >= 5000:
+            for chunk in divide_chunks(false_train_set, 5000):
+                if len(chunk) < 5000:
+                    false_train_set = set(chunk)
+                else:
+                    print('dumping...', c)
+                    with open(f'uspto/false_base/false_train_set/{c}.pickle', 'wb') as f:
+                        dump(chunk, f)
+                    print('dumping successful!')
+                    false_train_set = set()
+                    c += 1
+        if i == 917 and j == len(pickle_file) - 1:
+            print('dumping last time...')
+            with open(f'uspto/false_base/false_test_set/{a}.pickle', 'wb') as f:
+                dump(false_test_set, f)
+            with open(f'uspto/false_base/false_train_set/{t}.pickle', 'wb') as g:
+                dump(false_train_set, g)
+            print('dumping successful!')
 
-with open("test_train/false_test_set.pickle", 'wb') as m:
-    dump(false_test_set, m)
-print('false_test_set сохранился как false_test_set.pickle ')
+with open("uspto/false_base/false_train_molecules_signatures.pickle", 'wb') as m:
+    dump(false_train_molecules_signatures, m)
+print('false_train_molecules_signatures сохранился как false_train_molecules_signatures.pickle ')
 
-with open("test_train/false_test_molecules_ids.pickle", 'wb') as j:
-    dump(false_test_molecules_ids, j)
-print('false_test_molecules_ids был схранен как false_test_molecules_ids.pickle')
+with open("uspto/false_base/false_test_molecules_signatures.pickle", 'wb') as m:
+    dump(false_test_molecules_signatures, m)
+print('false_test_molecules_signatures сохранился как false_test_molecules_signatures.pickle ')
 
-with open("test_train/false_train_set.pickle", 'wb') as t:
-    dump(false_train_set, t)
-print('false_train_set был схранен как false_train_set.pickle')
-
-with open("test_train/false_train_molecules_ids.pickle", 'wb') as i:
-    dump(false_train_molecules_ids, i)
-print('false_train_molecules_ids был схранен как false_train_molecules_ids.pickle')
-
-with open("test_train/false_lost_set.pickle", 'wb') as o:
-    dump(false_lost_set, o)
-print('false_lost_set был схранен как false_lost_set_set.pickle')
-
-with open("test_train/false_lost_molecule_ids.pickle", 'wb') as n:
-    dump(false_lost_molecules_ids, n)
-print('false_lost_molecules_ids был сохранен как false_lost_molecules_ids.pickle')
-
-with open("test_train/intersection_dict.pickle", 'wb') as n:
-    dump(intersection_dict, n)
-print('intersection_dict был сохранен')
+print('количесвто тюплов потеряно false_lost_set', false_lost_set)
+print('пересекалось молекул false_train_molecules_signatures & false_test_molecules_signatures: ', y)
+print('пересекалось молекул false_train_molecules_signatures & true_test_molecules_signatures: ', u)
+print('пересекалось молекул false_test_molecules_signatures & true_train_molecules_signatures: ', p)
 
 end = time()
 print(end - start)
-
-print('колличество тюплов в false_test_set = {}'.format(len(false_test_set)))
-print('колличество тюплов в false_train_set = {}'.format(len(false_train_set)))
-print('пересекалось молекул false_train_molecules_ids & false_test_molecules_ids: ', a)
-print('пересекалось молекул false_train_molecules_ids & true_test_molecules_ids: ', b)
-print('пересекалось молекул false_test_molecules_ids & true_train_molecules_ids: ', c)
-print('количество тюплов потеряно: false_lost_set =', len(false_lost_set))
-
-logger.info('колличество тюплов в false_test_set = {}'.format(len(false_test_set)))
-logger.info('колличество тюплов в true_train_set = {}'.format(len(false_train_set)))
-
-# последние результаты:
-# колличество тюплов в false_test_set = 131636
-# колличество тюплов в false_train_set = 2056008
-# пересекалось молекул false_train_molecules_ids & false_test_molecules_ids:  0
-# пересекалось молекул false_train_molecules_ids & true_test_molecules_ids:  0
-# пересекалось молекул false_test_molecules_ids & true_train_molecules_ids:  0
-# количество тюплов потеряно: false_lost_set = 1373036
+#Последние резултаты:
+# количесвто тюплов потеряно false_lost_set 2007093
+# пересекалось молекул false_train_molecules_signatures & false_test_molecules_signatures:  0
+# пересекалось молекул false_train_molecules_signatures & true_test_molecules_signatures:  0
+# пересекалось молекул false_test_molecules_signatures & true_train_molecules_signatures: 0
+# 11532.240439414978
